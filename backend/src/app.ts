d1b16b0a1be178fc5bd2ed6fe24b43b4b2619786
enghddd <81799517+jung18@users.noreply.github.com>
2025-11-22 17:01:37 +0900

    backend init

---
+++ backend/src/app.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import http from "http";
+import cors from "cors";
+import path from "path";
+import ProfileRouter from "./profile/profileRouter";
+import GithubRouter from "./github/githubRouter";
+
+dotenv.config();
+
+const app = express();
+const httpServer = http.createServer(app);
+const httpPort = 8080;
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet({
+    crossOriginEmbedderPolicy: false,
+    contentSecurityPolicy: false
+}));
+
+app.use("/static", express.static(path.join(__dirname, "../public")));
+
+app.use("/api/profile", ProfileRouter);
+app.use("/api/gitgub", GithubRouter);
+
+
+httpServer.listen(httpPort, () => {
+    console.log(`http server started on ${httpPort}`);
+})
